a6989555f4d3fe8597ab1ac35741f06a76afa4ff
Prashant Mehta <prashant@invessence.com>
2015-12-10 01:54:58 +0000

    Yodlee Integration on Web

--- invessence/modules/core/src/main/java/com/invessence/converter/JavaUtil.java
+++ invessence/modules/core/src/main/java/com/invessence/converter/JavaUtil.java
@@ -67,4 +67,26 @@ public class JavaUtil implements Serializable
       }
    }
 
+   public static String UppercaseFirstLetters(String str)
+   {
+      if (str != null) {
+         boolean prevWasWhiteSp = true;
+         char[] chars = str.toCharArray();
+         for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetter(chars[i])) {
+               if (prevWasWhiteSp) {
+                  chars[i] = Character.toUpperCase(chars[i]);
+               }
+               prevWasWhiteSp = false;
+            } else {
+               prevWasWhiteSp = Character.isWhitespace(chars[i]);
+            }
+         }
+         return new String(chars);
+      }
+      else
+         return null;
+   }
+
+
 }
