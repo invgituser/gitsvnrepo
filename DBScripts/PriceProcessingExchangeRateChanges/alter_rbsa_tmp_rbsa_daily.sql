414b690ad4569270cc005aa1e268aadead40084e
Sunil Tiwari <sunil.tiwari@invessence.com>
2017-12-25 17:05:03 -0500

    Adding git-svn instructions to the README

--- DBScripts/PriceProcessingExchangeRateChanges/alter_rbsa_tmp_rbsa_daily.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE rbsa.tmp_rbsa_daily
-ADD COLUMN exchangeRate DOUBLE NULL AFTER adjusted_price,
-ADD COLUMN base_currency VARCHAR(3) NULL AFTER monthly_return;
--- Invessence/modules/invmodel/src/main/java/com/invmodel/risk/dao/RiskSaveSP.java
+++
@@ -1,8 +0,0 @@
-package com.invmodel.risk.dao;
-
-/**
- * Created by prashant on 11/10/2017.
- */
-public class RiskSaveSP
-{
-}
